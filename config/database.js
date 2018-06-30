6b27dbd8f8a1a187e375a214e77b000ee74167dd
Jason <haloos@me.com>
2018-06-30 11:06:01 -0400

    added routes

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://dbuser:dbpassword@ds119350.mlab.com:19350/exeter' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
